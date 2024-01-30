0fa55091cdc9eefb5fff5dac207fb102649a7e55
Alex <alex@nezvanov.ru>
2024-01-30 23:04:48 +0700

    feat: Переделал всё взаимодействие на WebSockets.

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,5 +1,108 @@
+import datetime
+from backend.utils import commit, add_and_commit
+from .models import Devices, Messages, Firmwares
 ALLOWED_EXTENSIONS = ['bin']
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+class DeviceConnection:
+    def __init__(self):
+        self.__device = None
+        self.is_authorized = False
+
+        
+    def update_last_connection(self):
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        self.__device.last_connection = datetime.datetime.utcnow()
+        commit()
+
+    def authorize(self, id:str, token:str, create_if_not_exist = True):  
+        if not id or not token or len(id) < 30 or len(token) < 30:
+            return False
+        device = Devices.query.filter(Devices.unique_id == id).first()
+        if not device:
+            if create_if_not_exist:
+                device = Devices(unique_id=id, token=token, last_connection=datetime.datetime.utcnow())
+                if not add_and_commit(device):
+                    raise Exception("Unable to create Device.")
+            else:
+                self.is_authorized = False
+                return False
+        elif device.token != token:
+            self.is_authorized = False
+            return False
+        self.__device = device
+        self.is_authorized = True
+        return True
+    
+    def get_message_device(self) :  
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        _message = None
+        message = Messages.query.filter(Messages.device == self.__device, Messages.from_device == False).order_by(Messages.date.desc()).first()
+        if message:
+            print(message.json)
+            _message = message.json
+            Messages.query.filter(Messages.device == self.__device, Messages.from_device == False, Messages.date <= message.date).delete()
+            commit()
+        return _message
+    
+    def get_message_server(self) :  
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        _message = None
+        message = Messages.query.filter(Messages.device == self.__device, Messages.from_device == True).order_by(Messages.date.desc()).first()
+        if message:
+            print(message.json)
+            _message = message.json
+            Messages.query.filter(Messages.device == self.__device, Messages.from_device == True, Messages.date <= message.date).delete()
+            commit()
+        return _message
+    
+    def post_message_device(self, message: dict) -> bool:  
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        message = Messages(from_device=True, device=self.__device, date=datetime.datetime.utcnow(), json=message)
+        if not add_and_commit(message):
+            return False
+        return True
+    
+    def post_message_server(self, message: dict) -> bool:  
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        message = Messages(from_device=False, device=self.__device, date=datetime.datetime.utcnow(), json=message)
+        if not add_and_commit(message):
+            return False
+        return True
+    
+    def post_system_info(self, system_info: dict) -> bool:  
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        self.__device.system_info = system_info
+        if not commit():
+            return False
+        return True
+    
+    def get_system_info(self) -> dict:  
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        return self.__device.system_info
+    
+    def get_firmware_version(self):  
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        _firmware = None
+        firmware = Firmwares.query.filter(Firmwares.device == self.__device).order_by(Firmwares.version.desc()).first()
+        if firmware:
+            _firmware = {'ver': firmware.version}
+        return _firmware
+    
+    def get_device_id(self) -> int:  
+        if not self.is_authorized:
+            raise Exception("UNAUTHORIZED REQUEST")
+        _firmware = None
+        return self.__device.id
+    
--- backend/api/views.py
+++ backend/api/views.py
@@ -14,7 +14,7 @@ from backend.utils import format_datetime, format_date, parse_date, leave_keys, 
 from sqlalchemy import or_, and_, union_all, union, select, literal_column, column, literal, text, desc, not_
 from .models import Devices, Messages, Firmwares
 import uuid
-from .utils import allowed_file
+from .utils import allowed_file, DeviceConnection
 
 api = Blueprint('api', __name__)
 
@@ -48,22 +48,22 @@ api = Blueprint('api', __name__)
 #     return make_response(jsonify(response), 200)
     
 
-@api.route('/server/<string:unique_id>/<string:token>', methods=["PUT"])
-def api_server_put(unique_id, token):
-    device = Devices.query.filter(Devices.unique_id == unique_id).first()
-    if not device:
-        return make_response(jsonify({'errors': ['Device not found.'], 'data': None}), 404)
-    elif device.token != token:
-        return make_response(jsonify({'errors': ['Incorrect token.'], 'data': None}), 401)
-    try:
-        json_data = request.get_json(force=True)
-    except Exception as err:
-        return make_response(jsonify({'errors': ['Incorrect JSON.'], 'data': None}), 400)
-    print(json_data)
-    message = Messages(from_device=False, device_id=device.id, date=datetime.datetime.utcnow(), json=json_data)
-    if not add_and_commit(message):
-        return make_response(jsonify({'errors': ['Unable to add message.'], 'data': None}), 500)
-    return make_response(jsonify({'errors': None, 'data': {}}), 200)
+# @api.route('/server/<string:unique_id>/<string:token>', methods=["PUT"])
+# def api_server_put(unique_id, token):
+#     device = Devices.query.filter(Devices.unique_id == unique_id).first()
+#     if not device:
+#         return make_response(jsonify({'errors': ['Device not found.'], 'data': None}), 404)
+#     elif device.token != token:
+#         return make_response(jsonify({'errors': ['Incorrect token.'], 'data': None}), 401)
+#     try:
+#         json_data = request.get_json(force=True)
+#     except Exception as err:
+#         return make_response(jsonify({'errors': ['Incorrect JSON.'], 'data': None}), 400)
+#     print(json_data)
+#     message = Messages(from_device=False, device_id=device.id, date=datetime.datetime.utcnow(), json=json_data)
+#     if not add_and_commit(message):
+#         return make_response(jsonify({'errors': ['Unable to add message.'], 'data': None}), 500)
+#     return make_response(jsonify({'errors': None, 'data': {}}), 200)
     
 
 @api.route('/firmware/<string:unique_id>/<string:token>', methods=["POST", "GET"])
@@ -114,78 +114,8 @@ def api_firmware_put(unique_id, token):
             return send_file(path, as_attachment=False)
         
 
-class DeviceConnection:
-    def __init__(self):
-        self.__device = None
-        self.is_authorized = False
-
-        
-    def update_last_connection(self):
-        if not self.is_authorized:
-            raise Exception("UNAUTHORIZED REQUEST")
-        self.__device.last_connection = datetime.datetime.utcnow()
-        commit()
-
-    def authorize(self, id:str, token:str):  
-        if not id or not token or len(id) < 30 or len(token) < 30:
-            return False
-        device = Devices.query.filter(Devices.unique_id == id).first()
-        if not device:
-            device = Devices(unique_id=id, token=token, last_connection=datetime.datetime.utcnow())
-            if not add_and_commit(device):
-                raise Exception("Unable to create Device.")
-        elif device.token != token:
-            self.is_authorized = False
-            return False
-        self.__device = device
-        self.is_authorized = True
-        return True
-    
-    def get_message(self) -> dict | None:  
-        if not self.is_authorized:
-            raise Exception("UNAUTHORIZED REQUEST")
-        _message = None
-        message = Messages.query.filter(Messages.device == self.__device, Messages.from_device == False).order_by(Messages.date.desc()).first()
-        if message:
-            print(message.json)
-            _message = message.json
-            Messages.query.filter(Messages.device == self.__device, Messages.from_device == False, Messages.date <= message.date).delete()
-            commit()
-        return _message
-    
-    def post_message(self, message: dict) -> bool:  
-        if not self.is_authorized:
-            raise Exception("UNAUTHORIZED REQUEST")
-        message = Messages(from_device=True, device=self.__device, date=datetime.datetime.utcnow(), json=message)
-        if not add_and_commit(message):
-            return False
-        return True
-    
-    def post_system_info(self, system_info: dict) -> bool:  
-        if not self.is_authorized:
-            raise Exception("UNAUTHORIZED REQUEST")
-        self.__device.system_info = system_info
-        if not commit():
-            return False
-        return True
-    
-    def get_firmware_version(self) -> dict | None:  
-        if not self.is_authorized:
-            raise Exception("UNAUTHORIZED REQUEST")
-        _firmware = None
-        firmware = Firmwares.query.filter(Firmwares.device == self.__device).order_by(Firmwares.version.desc()).first()
-        if firmware:
-            _firmware = {'ver': firmware.version}
-        return _firmware
-    
-    def get_device_id(self) -> int:  
-        if not self.is_authorized:
-            raise Exception("UNAUTHORIZED REQUEST")
-        _firmware = None
-        return self.__device.id
-
-@socket.route('/')
-def echo(ws):
+@socket.route('/api/server')
+def ws_server(ws):
     device = DeviceConnection()
     unauthorized_requests = 0
     while True:
@@ -199,22 +129,64 @@ def echo(ws):
                 continue
         # Запрашиваем авторизацию если она не пройдена
         if not device.is_authorized and not payload:
-            print(None, device.is_authorized, payload)
+            print("SERVER", None, device.is_authorized, payload)
+            unauthorized_requests += 1
+        elif not device.is_authorized and payload:
+            if device.authorize(payload.get("id", None), payload.get("token", None), False):
+                ws.send(json.dumps({"info": "AUTHORIZED"}))
+        elif device.is_authorized:
+            print("SERVER", device.get_device_id(), device.is_authorized, received_data)
+            device.update_last_connection()
+            _message = device.get_message_server()
+            if _message:
+                ws.send(json.dumps({'mes': _message}))
+
+            if payload:
+                if 'mes' in payload: # Обрабатываем сообщение
+                    device.post_message_server(payload.get('mes'))
+                if 'req' in payload: # Обрабатываем запрос данных
+                    requested_data = payload.get('req')
+                    if requested_data == 'SYSINFO': # Запрос версии прошивки
+                        ws.send(json.dumps({'system_info': device.get_system_info()}))
+
+
+        # Закрываем сокет если за 5 тактов не проведена авторизация
+        if unauthorized_requests >= 5:
+            ws.close()
+
+
+
+@socket.route('/api/device')
+def ws_device(ws):
+    device = DeviceConnection()
+    unauthorized_requests = 0
+    while True:
+        received_data = ws.receive(timeout=1)
+        payload = None
+        if received_data: 
+            try:
+                payload = json.loads(received_data)
+            except Exception:
+                ws.send(json.dumps({"err": "BAD_JSON"}))
+                continue
+        # Запрашиваем авторизацию если она не пройдена
+        if not device.is_authorized and not payload:
+            print("DEVICE", None, device.is_authorized, payload)
             unauthorized_requests += 1
         elif not device.is_authorized and payload:
             if device.authorize(payload.get("id", None), payload.get("token", None)):
                 ws.send(json.dumps({"info": "AUTHORIZED"}))
                 ws.send(json.dumps({"req": "SYSINFO"}))
         elif device.is_authorized:
-            print(device.get_device_id(), device.is_authorized, received_data)
+            print("DEVICE", device.get_device_id(), device.is_authorized, received_data)
             device.update_last_connection()
-            _message = device.get_message()
+            _message = device.get_message_device()
             if _message:
                 ws.send(json.dumps({'mes': _message}))
 
             if payload:
                 if 'mes' in payload: # Обрабатываем сообщение
-                    device.post_message(payload.get('mes'))
+                    device.post_message_device(payload.get('mes'))
                 if 'system_info' in payload: # Обрабатываем системную информацию
                     device.post_system_info(payload.get('system_info', {}))
                 if 'req' in payload: # Обрабатываем запрос данных
@@ -230,4 +202,6 @@ def echo(ws):
 
 
 
+
+    
     
